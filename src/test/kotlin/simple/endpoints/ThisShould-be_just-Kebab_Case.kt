b0e054e1a399c698147c0b0a3a9d0c4895aecf00
RORAK <rorakcodes@gmail.com>
2024-07-16 19:17:29 +0200

    warning fix

--- src/test/kotlin/simple/endpoints/ThisShould-be_just-Kebab_Case.kt
+++ src/test/kotlin/simple/endpoints/ThisShould-be_just-Kebab_Case.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 
+@Suppress("ClassName")
 class `ThisShould-be_just-Kebab_Case`: BKRoute {
 	override suspend fun get(call: ApplicationCall, request: ApplicationRequest) {
 		call.respondText("This Should Be Just Kebab Case");
